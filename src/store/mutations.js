c916d04d21d4ee3a7e29176a07bcbdc089cd7e5d
LeungChiHo <510769688@qq.com>
2018-06-26 10:41:29 +0800

    修改注释

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,10 +1,5 @@
 import * as types from './mutation-types'
 
-// 对于vuex的用法，其实理解了流程就行
-// vuex -> actions -> 提交（commit）mutations ->state -> view -> dispatch 触发 actions ->...
-// actions 其实是mutations的升级版，它实现了mutations只能同步改变状态不能异步改变
-// actions 就是可以异步操作mutation的提交
-// 具体可以看下我的blog中的总结 http://selvinpro.com/2017/03/17/vuex-about/#more
 export const mutations = {
   // 这里的data指提交时：
   // 从/api/login传回的user对象，其中包含name,messeage等信息
