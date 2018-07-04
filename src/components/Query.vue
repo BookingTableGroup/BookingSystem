e26ae3110c8e7acb63fb1fdcf5307b61de520f0d
VolcanoLiuY <654966197@qq.com>
2018-07-04 12:49:59 +0800

    final style (add the delete function

--- src/components/Query.vue
+++ src/components/Query.vue
@@ -61,6 +61,7 @@
     <br/>
   </section>
 </template>
+
 <script>
 import item from './item.vue'
 export default {
@@ -68,10 +69,6 @@ export default {
     item
   },
   data () {
-    this.colConfigs = [
-      { prop: 'price', label: '价格' },
-      { prop: 'name', label: '菜名' }
-    ]
     return {
       total: 0,
       tableData: [],
@@ -95,9 +92,44 @@ export default {
       );
     },
     computeTotal: function () {
-       for (let index = 0; index <  this.tableData[0].foods.length; index++) {
-        this.total = this.tableData[0].foods[index].price + this.total;
+       for (let index = 0; index <  this.tableData.foods.length; index++) {
+        this.total = this.tableData.foods[index].price + this.total;
       }
+    },
+    deleteOrder: function() {
+      var that = this;
+      var myphone = localStorage.getItem('user');
+
+      this.$confirm("删除此订单, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$http.put(`/api/deleteMyOrder/${myphone}`).then(
+            function(response) {
+              if (response.ok) {
+                this.$message({
+                  type: "success",
+                  message: "删除订单成功!"
+                });
+                this.total = 0;
+                that.getAll();
+              } else {
+                this.$message({
+                  type: "error",
+                  message: "删除订单失败!"
+                });
+              }
+            }
+          );
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
     }
   },
     //页面初始化进来查询数据
@@ -113,13 +145,56 @@ export default {
     width: 80%;
     /* border: 2px solid rgb(110, 86, 7); */
   }
-   *{
+  *{
     margin: 0;
     padding: 0;
   }
-
-
-
+  #menu_icon {
+    height: 50px;
+    width: 50px;
+  }
+  #line_query_3 {
+    margin-top:-15px;
+    margin-left: 1380px;
+    width:30%;
+    height:3px;
+    background:rgb(051, 069, 091);
+  }
+  #line_query_2 {
+    margin-top:300px;
+    margin-left: 200px;
+    width:30%;
+    height:3px;
+    background:rgb(051, 069, 091);
+  }
+  #line_query {
+    margin-top:80px;
+    margin-left: 200px;
+    width:80%;
+    height:2px;
+    background:rgb(051, 069, 091);
+  }
+  #query_but {
+    margin-top: -20px;
+    margin-left: 1050px;
+    background-color:rgb(051, 069, 091);
+    width: 200px;
+    height: 35px;
+    box-shadow: #666 0px 0px 10px;
+  }
+  #ellipse_query {
+    box-shadow: #666 0px 0px 10px;
+    width: 150px;
+    height: 150px;
+    margin-top: 30px;
+    margin-left: 150px;
+    border: 4px solid rgb(051, 069, 091);
+    border-radius: 50%;
+  }
+  #ellipse_query img {
+    width: 100%;
+    height:100%;
+  }
   #container {
     height: 400px;
     /* border: 1px solid blue; */
@@ -130,91 +205,104 @@ export default {
     width: 20%;
   }
   #curOrder {
-    float: left;
+    margin-left: 450px;
     height: 40px;
     width: 60%;
-    background-color: rgb(000, 188, 212);
-    color: white;
-    font-size: 25px;
+    color: rgb(051, 069, 091);
+    font-size: 40px;
     font-family: Roboto;
-    box-shadow: #666 0px 0px 5px;
-    margin-top: 40px;
+    margin-top: 30px;
   }
-
-  #tableInfo {
+  #table_type_query {
+    background-image: url('../assets/bg3.jpeg');
+    background-size:cover;
     margin-top: 40px;
+    margin-left: 100px;
     background-color: white;
     float: left;
-    height: 400px;
-    width: 40%;
-    box-shadow: #666 0px 0px 5px;
-    border:  3px solid rgb(000, 188, 212);
+    height: 600px;
+    width: 20%;
+    box-shadow: #666 0px 0px 10px;
+    border:  3px solid rgb(051, 069, 091);
   }
-  #foodTitle{
+  #tableInfo {
+    background-image: url('../assets/bg1.png');
     margin-top: 40px;
+    margin-left: 350px;
+    background-color: white;
     float: left;
-    width: 100%;
-    height: 100px;
-    color: black;
-    font-size:120px;
-    font-family: Roboto;
+    height: 600px;
+    width: 20%;
+    box-shadow: #666 0px 0px 10px;
+    border:  3px solid rgb(051, 069, 091);
+  }
+  #table_food_query {
+    background-image: url('../assets/bg2.jpeg');
+    margin-top: 40px;
+    margin-left: 100px;
+    background-color: white;
+    float: left;
+    height: 600px;
+    width: 20%;
+    box-shadow: #666 0px 0px 10px;
+    border:  3px solid rgb(051, 069, 091);
   }
   #deleteIcon{
     width: 30px;
   }
   #tableMem {
-    margin-top: 20px;
-    float: left;
-    width: 100%;
-    height: 150px;
-    border: 3px solid white;
-    /*box-shadow: #666 0px 0px 5px;*/
-    color: rgba(102, 93, 102, 1);
+    text-shadow: #666 0px 0px 10px;
+    color: rgb(051, 069, 091);
+    font-size: 35px;
     font-family: Roboto;
-  }
-  #table8{
-    float: left;
-    height: 145px;
-    width: 240px;
-    box-shadow: #666 0px 0px 10px;
-    /*border:  3px solid rgb(000, 188, 212);*/
+    margin-top: 50px;
+    margin-left: -10px;
+    text-decoration: underline;
   }
   #tableId{
-    color:black;
-    margin-top: 50px;
+    text-shadow: #666 0px 0px 10px;
+    color: rgb(051, 069, 091);
     font-size: 35px;
-    margin-right: -35px;
+    font-family: Roboto;
+    margin-top: 100px;
+    margin-left: -10px;
+    text-decoration: underline;
   }
   #tableTime {
-    float: right;
-    margin-top: 33px;
-    margin-right: 46%;
-    width: 50%;
-    color:black;
-    font-size: 22px;
+    text-shadow: #666 0px 0px 10px;
+    color: rgb(051, 069, 091);
+    font-size: 35px;
     font-family: Roboto;
+    margin-top: 50px;
+    margin-left: -10px;
+    text-decoration: underline;
   }
   #tablePhone {
-    float: right;
-    margin-top: 30px;
-    margin-left: 60%;
-    color:black;
-    font-size: 20px;
+    color: rgb(051, 069, 091);
+    font-size: 35px;
+    font-family: Roboto;
+    margin-top: 50px;
+    margin-left: 20px;
+    text-decoration: underline;
   }
-  #foodTitle {
-    float: right;
-    height: 30px;
-    color:black;
-    margin-right:-120px;
-    font-size:35px;
+  #foodInfo {
+    text-shadow: #666 0px 0px 10px;
+    text-decoration: underline;
+    float:left;
+    margin-top: 40px;
+    margin-left: -15px;
+    height: 500px;
+    width: 100%;
+    color:rgb(051, 069, 091);
     font-family: Roboto;
   }
-
   .foodList {
-    margin-left: 150px;
-    width: 40%;
-    box-shadow: #666 0px 0px 5px;
-    border:  3px solid rgb(000, 188, 212);
+    box-shadow: #666 0px 0px 10px;
+    font-size: 25px;
+    margin-left: 85px;
+    width: 70%;
+    height: 300px;
+    /*box-shadow: #666 0px 0px 5px;*/
+    border:  3px solid rgb(051, 069, 091);
   }
-
 </style>
